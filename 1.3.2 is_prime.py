d6028ca645473612aa4f5fddf9efcc2ec60d9d80
Yael Chen <51058584+YaelChen@users.noreply.github.com>
2025-01-07 13:14:29 +0200

    Update 1.3.2 is_prime.py

--- 1.3.2 is_prime.py
+++ 1.3.2 is_prime.py
@@ -1,6 +1,9 @@
+""" This function is going through all the numbers to the given number.
+A better version is at "Prime_nums-efficient.py", checking only the numbers up to the given num square root. """
+
 def is_prime(number):
     """
-    A one liner function that checks if a given number is prime.
+    A one-liner function that checks if a given number is prime.
     :param number: a given number
     :type number: int
     :return: True/False
